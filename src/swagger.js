578d7b2161471438855d3b5c5d8bc79430a21b99
Theodorus Yoga <theodorus.mahendraputra@modalrakyat.id>
2019-09-30 18:12:34 +0700

    Added ESLint with Airbnb configuration

--- src/swagger.js
+++ src/swagger.js
@@ -5,10 +5,10 @@ const options = {
     openapi: '3.0.0',
     info: {
       title: 'Xendit API Documentation',
-      version: '1.0.0'
-    }
+      version: '1.0.0',
+    },
   },
-  apis: ['./src/routes.js']
+  apis: ['./src/routes.js'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
